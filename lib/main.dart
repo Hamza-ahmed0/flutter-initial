26e7af654fb39ed645d243632ff5a023198ab04b
hamza ahmed <ham342791@gmail.com>
2021-05-29 13:25:36 -0700

    flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Text("HAMZA AHMED, ham342791@gmail.com"),
-      ),
-      
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Center(
+                child: Text("LOGIN"),
+              ),
+            ),
+            body: Center(
+              child: Container(
+                color: Colors.lightBlue[50],
+                width: 250,
+                height: 400,
+                child: Column(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  SizedBox(height: 50),
+                  Container(
+                      width: 200,
+                      color: Colors.white,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Username'),
+                      )),
+                    SizedBox(height: 20,),
+                   Container(
+                     color: Colors.white,
+                      width: 200,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Password'),
+                      )),
+                      SizedBox(height: 20,),
+                      ElevatedButton(onPressed: (){}, child: Text("Login"))
+                ]),
+              ),
+            )));
   }
 }
